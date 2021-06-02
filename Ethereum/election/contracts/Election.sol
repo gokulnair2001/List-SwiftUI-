6fc307aa6603528b26d7e0b75ae621af3fa3c675
gokulnair2001 <gokulnair.2001@gmail.com>
2021-06-02 17:51:32 +0530

    first commit

---
+++ Ethereum/election/contracts/Election.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract Election {
+     // Store andidate
+     // Read Candidate
+     string public candidate;
+     // Constructor
+    function Elections () public {
+            candidate = "Candidate 1"
+    }
+}
