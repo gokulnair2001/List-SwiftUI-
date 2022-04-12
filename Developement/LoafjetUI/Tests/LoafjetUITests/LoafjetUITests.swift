47207df58694a023ec2f25a4009296731e8bf43b
gokulnair2001 <gokulnair.2001@gmail.com>
2022-04-12 12:02:16 +0530

    Initial Commit

---
+++ Developement/LoafjetUI/Tests/LoafjetUITests/LoafjetUITests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import LoafjetUI
+
+final class LoafjetUITests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(LoafjetUI().text, "Hello, World!")
+    }
+}
